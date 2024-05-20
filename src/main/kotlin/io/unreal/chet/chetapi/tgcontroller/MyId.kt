9e9c1b0dcadb091c0591861bdd1f3266d5ec912f
Ariel Saldana <ariel@unreal.io>
2024-05-20 13:02:49 -0400

    Update id service to include finding other IDs

--- src/main/kotlin/io/unreal/chet/chetapi/tgcontroller/MyId.kt
+++ src/main/kotlin/io/unreal/chet/chetapi/tgcontroller/MyId.kt
@@ -2,6 +2,7 @@ package io.unreal.chet.chetapi.tgcontroller
 
 import eu.vendeli.tgbot.TelegramBot
 import eu.vendeli.tgbot.annotations.CommandHandler
+import eu.vendeli.tgbot.annotations.RegexCommandHandler
 import eu.vendeli.tgbot.api.message.message
 import eu.vendeli.tgbot.types.User
 import eu.vendeli.tgbot.types.internal.MessageUpdate
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Component
 class MyId() {
     companion object {
         const val MY_ID_COMMAND = "/id"
+        const val FIND_ID_COMMAND = "/fid"
+        const val FAILED_TO_FIND_USERID_ERROR_MESSAGE = "Failed to get user id"
+        const val NULL_STRING = "null"
     }
 
     @CommandHandler([MY_ID_COMMAND])
@@ -18,5 +22,24 @@ class MyId() {
         message { user.id.toString() }
             .options { replyParameters(messageId = up.message.messageId) }
             .send(up.message.chat.id, bot)
+    }
+
+    @RegexCommandHandler("^$FIND_ID_COMMAND .*")
+    suspend fun findId(bot: TelegramBot, up: MessageUpdate, user: User) {
+        var failedToGetUserId = true
+        val userId = up.message.entities!![1].user?.id.toString()
+
+        if (userId.isNotEmpty() && userId != NULL_STRING) {
+            message { userId }
+                .options { replyParameters(messageId = up.message.messageId) }
+                .send(up.message.chat.id, bot)
+            failedToGetUserId = false
+        }
+
+        if (failedToGetUserId) {
+            message { FAILED_TO_FIND_USERID_ERROR_MESSAGE  }
+                .options { replyParameters(messageId = up.message.messageId) }
+                .send(up.message.chat.id, bot)
+        }
     }
 }
