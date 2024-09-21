031f92734a944dbb05babe2a92e2e2994cc14511
Ariel Saldana <ariel@unreal.io>
2024-09-20 22:02:38 -0400

    Cleanup PromptController.kt

--- src/main/kotlin/io/unreal/chet/chetapi/controller/PromptController.kt
+++ src/main/kotlin/io/unreal/chet/chetapi/controller/PromptController.kt
@@ -9,21 +9,6 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/prompt")
 class PromptController(
     val userRepository: UserRepository,
-//    val userByTelegramIdRepository: UserByTelegramIdRepository,
     private val promptService: PromptService
 ) {
-
-//    @PostMapping("/text")
-//    suspend fun getChatPrompt(@RequestBody request: PromptRequest): Mono<ResponseEntity<HttpResponse>> {
-//        return promptService.processPrompt(request, 5)
-//            .map { result ->
-//                val response = HttpResponse(
-//                    error = null, success = SimpleStringResponseEntity(result)
-//                )
-//                ResponseEntity.ok(response)
-//            }
-//            .onErrorResume { error ->
-//                Mono.just(ResponseEntity.status(400).body(HttpResponse(error = error.message, success = null)))
-//            }
-//    }
 }
